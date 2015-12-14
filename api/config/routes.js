59adf3202f10ce9e9ba7d5b9dbc7641863ddbd88
KwakesProject <kwakes@me.com>
2015-12-14 12:36:25 +0000

    routes.js set up and added controllers folder with files

---
+++ api/config/routes.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+var producersController = require('../controllers/producersController');
+var songsController = require('../controllers/songsController');
+
+// Producer Routes
+/*router.route('/')
+  .get(producersController.producersIndex)*/
+
+router.route('/producers')
+  .get(producersController.producersIndex)
+  .post(producersController.producersCreate)
+
+router.route('/producers/:id')
+  .get(producersController.producersShow)
+  .patch(producersController.producersUpdate)
+  .delete(producersController.producersDelete)
+
+// Song Routes
+router.route('/songs')
+  .get(songsController.songsIndex)
+  .post(songsController.songsCreate)
+
+router.route('/songs/:id')
+  .get(songsController.songsShow)
+  .patch(songsController.songsUpdate)
+  .delete(songsController.songsDelete)
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
