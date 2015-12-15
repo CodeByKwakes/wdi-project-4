9624d687713cb919911dffc35b334e2b52c18574
KwakesProject <kwakes@me.com>
2015-12-15 15:41:39 +0000

    after a long time not getting anything to display, i have finally got producer details showing on localhost 8000

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,2 @@
 angular
-  .module("BeatSity", ['ngResource', 'ui.router'])
-  .constant('API', 'http://localhost:3000/api')
-  .config(MainRouter)
-  
-MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
-
-function MainRouter($stateProvider, $urlRouterProvider){
-  
-}
+  .module("BeatSity", []);
--- frontend/js/controllers/producersController.js
+++ frontend/js/controllers/producersController.js
@@ -2,9 +2,9 @@ angular
   .module('BeatSity')
   .controller('ProducersController', ProducersController);
 
-  ProducersController.$inject = [];
+  ProducersController.$inject = ['$http'];
 
-  function ProducersController(){
+  function ProducersController($http){
     var self            =  this;
 
     self.all            = [];
@@ -13,9 +13,42 @@ angular
     self.getProducers   = getProducers;
     self.addProducer    = addProducer;
     self.deleteProducer = deleteProducer;
+    self.producerReset  = producerReset;
 
-  }
+    function getProducers(){
+      $http
+        .get('http://localhost:3000/api/producers')
+        .then(function(response){
+          self.all = response.data.producers;
+          console.log(self.all);
+          console.log(response)
+        });
+    }
+    
+    function addProducer(){
+    $http
+      .post('http://localhost:3000/api/producers', { producer: self.newProducer })
+      .then(function(response){
+        self.all.push(self.newProducer);
+      });
+    }
 
-  function getProducers(){}
-  function addProducer(){}
-  function deleteProducer(){}
+    function deleteProducer(producer){
+    $http
+      .delete('http://localhost:3000/producers/' + producer._id)
+      .then(function(response){
+        var index = self.all.indexOf(producer);
+        self.all.splice(index, 1);
+
+      });
+    }
+
+    function producerReset(){
+      self.producer = {};
+      console.log(self.producer);
+    }
+    
+  getProducers();
+}
+
+  
